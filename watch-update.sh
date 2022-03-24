faabbc0e8c52cef3f2791278cc68bba6ccaab239
damiancurry <damiancurry@gmail.com>
2022-03-24 09:35:39 -0700

    updated scripts to set hostnames

--- watch-update.sh
+++ watch-update.sh
@@ -7,17 +7,20 @@ runnumber=$1
 appname=$2
 servername=$3
 #uripath=$4
-namespace='f5-nginx-canary-deployments'
+namespace='nginx-ingress'
 oldsvc=`kubectl describe virtualserver $appname -n $namespace | grep Service | awk '{ print $2}'` 
 newsvc="${appname}-svc-${runnumber}"
 #replacenames='${oldsvc} ${newsvc} '
 oldweight=50
 newweight=50
+ichostname=`kubectl get svc -A | grep ingress | awk '{print $5}'`
+echo $ichostname
+#IC_IP=`getent ahostsv4 $ichostname | grep STREAM | head -n 1 | cut -d ' ' -f 1`
 
 #deploy virtualserver with header match
 echo "Deploying ${newsvc} with beta header"
 echo $oldsvc $newsvc
-sed "s|existing-svc|${oldsvc}|g" kic/header-split.yaml | sed "s|new-svc|${newsvc}|g" > header-split.yaml
+sed "s|existing-svc|${oldsvc}|g" kic/header-split.yaml | sed "s|new-svc|${newsvc}|g" | sed "s|elb-hostname|${ichostname}" > header-split.yaml
 cat header-split.yaml
 kubectl apply -f header-split.yaml --namespace $namespace
 
@@ -25,40 +28,42 @@ kubectl apply -f header-split.yaml --namespace $namespace
 #check app is running fine with custom headers
 ichostname=`kubectl get svc -A | grep ingress | awk '{print $5}'`
 echo $ichostname
-IC_IP=`getent ahostsv4 $ichostname | grep STREAM | head -n 1 | cut -d ' ' -f 1`
+#IC_IP=`getent ahostsv4 $ichostname | grep STREAM | head -n 1 | cut -d ' ' -f 1`
 
 sleep 30
 echo $IC_IP
-headerreturncode=`curl -i -s -o /dev/null -w "%{http_code}" -H "release: beta" --resolve demo.example.com:80:$IC_IP http://demo.example.com/`
+headerreturncode=`curl -i -s -o /dev/null -w "%{http_code}" -H "release: beta" https://$ichostname/`
 echo $headerreturncode
 if [ $headerreturncode == '200' ]
 then
 	echo "Header tests passed. Moving on"
 else
 	echo "Check failed, rolling back version"
-    sed "s|new-svc|${oldsvc}|g" kic/single-svc-virtualserver.yaml > single-svc-virtualserver.yaml
+    sed "s|new-svc|${oldsvc}|g" kic/single-svc-virtualserver.yaml | sed "s|elb-hostname|${ichostname}"  > single-svc-virtualserver.yaml
     kubectl apply -f single-svc-virtualserver.yaml --namespace $namespace
 	exit 1
 fi
 
 #assuming checks work, deploy virtualserver with 50/50 split for new/old
-sed "s|existing-svc|${oldsvc}|g" kic/weight-split.yaml | sed "s|new-svc|${newsvc}|g" | sed "s|new-weight|${newweight}|g" | sed "s|old-weight|${oldweight}|g" > weight-split.yaml
+sed "s|existing-svc|${oldsvc}|g" kic/weight-split.yaml | sed "s|new-svc|${newsvc}|g" | sed "s|new-weight|${newweight}|g" | sed "s|old-weight|${oldweight}|g" | sed "s|elb-hostname|${ichostname}" > weight-split.yaml
 cat weight-split.yaml
 kubectl apply -f weight-split.yaml --namespace $namespace
+
+
+#generate some traffic
+c=1
+while [[ $c -le 50 ]]
+do 
+   curl -i -s -o /dev/null https://$ichostname/foo
+   #curl -i -s -o /dev/null https://$ichostname/
+   let c=c+1
+done
 
 #create port forwarding tunnel
 ingresspod=`kubectl get pods -A | grep ingress | awk '{print $2}'`
 kubectl port-forward $ingresspod 8080:8080 --namespace=default &
 portforwardpid=$!
 sleep 15
-#generate some traffic
-c=1
-while [[ $c -le 50 ]]
-do 
-   curl -i -s -o /dev/null --resolve demo.example.com:80:$IC_IP http://demo.example.com/foo
-   #curl -i -s -o /dev/null --resolve demo.example.com:80:$IC_IP http://demo.example.com/
-   let c=c+1
-done
 #check the status code returns 
 checkurl="http://localhost:8080/api/6/http/upstreams/vs_${namespace}_${appname}_${newsvc}"
 echo ${checkurl}
@@ -74,13 +79,13 @@ then
 	echo "live traffic tests passed. Moving on"
 else
 	echo "Check failed, rolling back version"
-    sed "s|new-svc|${oldsvc}|g" kic/single-svc-virtualserver.yaml > single-svc-virtualserver.yaml
+    sed "s|new-svc|${oldsvc}|g" kic/single-svc-virtualserver.yaml | sed "s|elb-hostname|${ichostname}"  > single-svc-virtualserver.yaml
     kubectl apply -f single-svc-virtualserver.yaml --namespace $namespace
 	exit 1
 fi
 #assuming checks return ok, depricate old version and push all traffic to new
 echo "Rolling back old version of the application"
-sed "s|new-svc|${newsvc}|g" kic/single-svc-virtualserver.yaml > single-svc-virtualserver.yaml
+sed "s|new-svc|${newsvc}|g" kic/single-svc-virtualserver.yaml | sed "s|elb-hostname|${ichostname}"  > single-svc-virtualserver.yaml
 kubectl apply -f single-svc-virtualserver.yaml --namespace $namespace
 exit 0
 
